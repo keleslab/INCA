dce53d4cae347b3536a03ff85c6bed43fc83b643
jduan607 <48993019+jduan607@users.noreply.github.com>
2023-10-17 20:37:03 -0500

    Add files via upload

--- R/INCA.R
+++ R/INCA.R
@@ -21,7 +21,7 @@ convertToRLE = function(input) {
     }
     rle_object = input
   } else {
-    print('Invalid: `input` need to be either a data.frame or a list of RLE or data.frame.')
+    message('Invalid: `input` need to be either a data.frame or a list of RLE or data.frame.')
   }
 
   return(rle_object)
@@ -31,18 +31,18 @@ convertToRLE = function(input) {
 extractPeaksBySignals = function(peaks, threshold=0.5) {
   idr_col = grep('IDR', colnames(peaks), ignore.case=TRUE) ## column of IDR peaks
   if (length(idr_col) == 0) {
-    print('Column of IDR peaks not found. Return the original file.')
+    message('Column of IDR peaks not found. Return the original file.')
     return(peaks)
   }
   if (length(idr_col) > 1) {
-    print('Multiple columns of IDR peaks. Use the first column.')
+    message('Multiple columns of IDR peaks. Use the first column.')
     idr_col = idr_col[1]
   }
 
   signal_col = grep('signal(?!.*IDR)', colnames(peaks), ignore.case=TRUE, perl=TRUE) ## columns of non-IDR peaks
 
   if (threshold < 0 | threshold > 1) {
-    print('Invalid quantile for `threshold`. Use default = 0.5.')
+    message('Invalid quantile for `threshold`. Use default = 0.5.')
     threshold = 0.5
   }
 
@@ -62,11 +62,11 @@ readCountsWRTControl = function(experiment, control, method=1, pseudocount=0.1) 
   rle_ctrl = convertToRLE(control)
 
   if (!method %in% c(1,2)) {
-    print('Invalid `method` option. Use default = 1 (subtraction).')
+    message('Invalid `method` option. Use default = 1 (subtraction).')
     method = 1
   }
   if (method == 2 & pseudocount <= 0) {
-    print('Invalid `pseudocount` for fold change. Use default = 0.1.')
+    message('Invalid `pseudocount` for fold change. Use default = 0.1.')
     pseducount = 0.1
 
   }
@@ -96,7 +96,7 @@ findReadCountsCutoff = function(counts, peaks, threshold=0.5, parallel=FALSE) {
   }
 
   if (threshold < 0 | threshold > 1) {
-    print('Invalid quantile for `threshold`. Use default = 0.5.')
+    message('Invalid quantile for `threshold`. Use default = 0.5.')
     threshold = 0.5
   }
   cutoff = quantile(as.vector(peaks_RC), threshold)
@@ -132,10 +132,10 @@ varInEnRegionByCounts = function(vdata, counts, cutoff, parallel=FALSE) {
 
 varInEnrichedRegion = function(variants, input, window=15, parallel=FALSE) {
   if (window < 0) {
-    print('Invalid `window` size. Use default = 15.')
+    message('Invalid `window` size. Use default = 15.')
     window = 15
   }
-  data = variants[, .(Chr,Start,End)]
+  data = as.data.table(variants)[, .(Chr,Start,End)]
   data[, ':='(Chr = standardizeChr(Chr), Start = Start - window, End = End + window)]
 
   if (!all(sapply(input, function(x) 'list' %in% class(x)))) {
@@ -149,18 +149,18 @@ varInEnrichedRegion = function(variants, input, window=15, parallel=FALSE) {
         if (cases[2]) { #### peaks provided, findReadCountsCutoff()
           ls$cutoff = findReadCountsCutoff(ls$counts, ls$peaks, ifelse(cases[4],ls$threshold,-1))
         } else {
-          print('`cutoff` not provided. Use default cutoff = 30.')
+          message('`cutoff` not provided. Use default cutoff = 30.')
           ls$cutoff = 30
         }
       }
       varInEnRegionByCounts(data, ls$counts, ls$cutoff, parallel)
     } else if (cases[2]) { ## varInEnRegionByPeaks()
-      if (cases[4]) { #### threshold provided, extractPeaksBySignals()
+      if (cases[4]) { ### threshold provided, extractPeaksBySignals()
         ls$peaks = extractPeaksBySignals(ls$peaks, ls$threshold)
       }
       varInEnRegionByPeaks(data, ls$peaks)
     } else {
-      print('No files for `peaks` or `counts` provided.')
+      message('No files for `peaks` or `counts` provided.')
       return(NULL)
     }
   }
@@ -169,19 +169,191 @@ varInEnrichedRegion = function(variants, input, window=15, parallel=FALSE) {
 }
 
 
-computeAllelicEffect = function(variants, CellLine1, CellLine2, CellLineNames, window=15, parallel=FALSE) {
-  enriched = matrix(0, ncol=2, nrow=nrow(variants))
+mapCellGenoToVar = function(variants, CellLine1, CellLine2, CellNames) {
+  variants = as.data.table(variants)
+
+  if (!'Zyg' %in% colnames(variants)) {
+    variants[,'Zyg'] = 1
+    message('Zygosity not provided. Assume all variants are heterozygous.')
+  }
 
   ## Cell line 1
-  indices = varInEnrichedRegion(variants, CellLine1, window, parallel)
-  enriched[indices,1] = 1
-
+  colnames(CellLine1) = c('Chr','Pos','Ref', paste('C1',c('Alt','Alt2','Zyg'),sep='_'))
+  CellLine1 = CellLine1[variants[,.(Chr,Pos,Ref)], on=.(Chr=Chr, Pos=Pos, Ref=Ref)][,-c(1:3)]
   ## Cell line 2
-  indices = varInEnrichedRegion(variants, CellLine1, window, parallel)
-  enriched[indices,2] = 1
+  colnames(CellLine2) = c('Chr','Pos','Ref', paste('C2',c('Alt','Alt2','Zyg'),sep='_'))
+  CellLine2 = CellLine2[variants[,.(Chr,Pos,Ref)], on=.(Chr=Chr, Pos=Pos, Ref=Ref)][,-c(1:3)]
+
+  type = paste(apply(CellLine1, 1, function(x) any(!is.na(x))*1),
+               apply(CellLine2, 1, function(x) any(!is.na(x))*1), sep=':')
+
+  cdata = cbind(variants[,.(Alt,Zyg)], CellLine1, CellLine2, AltCellLine='Neither')
+  cdata[type=='1:0' & Alt==C1_Alt & C1_Alt2=='', 'AltCellLine'] = CellNames[1]
+  cdata[type=='0:1' & Alt==C2_Alt & C2_Alt2=='', 'AltCellLine'] = CellNames[2]
+
+  cdata[type=='1:1' & Alt==C1_Alt & Alt==C2_Alt, 'AltCellLine'] = 'Both'
+  cdata[AltCellLine=='Both' & Zyg==C1_Zyg & Zyg!=C2_Zyg, 'AltCellLine'] = CellNames[1]
+  cdata[AltCellLine=='Both' & Zyg==C2_Zyg & Zyg!=C1_Zyg, 'AltCellLine'] = CellNames[2]
+
+  cdata[C1_Alt2!='', C1_Alt := paste(C1_Alt,C1_Alt2,sep=',')]
+  cdata[C2_Alt2!='', C2_Alt := paste(C2_Alt,C2_Alt2,sep=',')]
+
+  cdata = cdata[,.(C1_Alt,C1_Zyg,C2_Alt,C2_Zyg,AltCellLine)]
+  colnames(cdata) = gsub('C1',CellNames[1],colnames(cdata))
+  colnames(cdata) = gsub('C2',CellNames[2],colnames(cdata))
+
+  variants = cbind(variants, cdata)
+  return(variants)
+}
+
+
+mapDEResultToGenes = function(variants, DEresult, CellName=NULL) {
+  data = as.data.table(variants)[,'Gene']
+  suppressMessages(
+    suppressWarnings(
+      data[, Gene := HGNChelper::checkGeneSymbols(Gene,unmapped.as.na=FALSE)[,'Suggested.Symbol']]))
+
+  DEresult = as.data.table(DEresult)
+  DEresult = DEresult[, .(gene = unlist(strsplit(gene, '[^[:alnum:]\\-\\_\\.]', perl=TRUE))),
+                      by=eval(colnames(DEresult)[-1])]
+  suppressMessages(
+    suppressWarnings(
+      DEresult[, gene := HGNChelper::checkGeneSymbols(gene,unmapped.as.na=FALSE)[,'Suggested.Symbol']]))
+
+  DEresult = DEresult[, .SD[which.min(1)], by = gene] ## 1: column of p-value/q-value
+  DEresult[, 'significant'] = (DEresult[[2]] <= 0.05) * 1 ## 2: column of p-value/q-value
+  data = DEresult[data, on=.(gene=Gene)][,-1] ## match to data
+
+  if (!is.null(CellName)) {
+    colnames(data) = paste(CellName, colnames(data), sep='_')
+  }
+  return(data)
+}
+
+
+mapSWToVariants = function(variants, SW) {
+  variants = as.data.table(variants)
+  SW = as.data.table(SW)
+
+  SW[, pos := as.numeric(pos)]
+  SW = SW[variants, on=.(chrom=Chr, pos=Pos, ref=Ref, alt=Alt), mult='first'][, 1:ncol(SW)]
+  return(SW)
+}
+
+scoreClinVarQSW = function(variants, SW, target, pathogenic=TRUE) {
+  variants = as.data.table(variants)
+  SW = as.data.table(SW)
+
+  SW = mapSWToVariants(variants,SW)
+  SW = SW[,grep(paste0(target,'[|]'), colnames(SW)),with=FALSE]
+  colnames(SW) = gsub('(.+?)[|](.+?)[|].*rep(.*).hg19','\\2_\\1_SW_Rep\\3', colnames(SW)) # reformat colnames
+  variants = cbind(variants, SW)
+
+  ## Load quantiles of SeqWeaver from ClinVar
+  directory = 'https://raw.github.com/jduan607/INCA/master/ClinVar_SeqWeaver_scores'
+  if (pathogenic) {
+    clinvar = fread(file.path(directory,'clinvar_pathogenic_snv_sw_quantile.txt.gz'))
+  } else {
+    clinvar = fread(file.path(directory,'clinvar_snv_sw_quantile.txt.gz'))
+  }
+  clinvar = clinvar[order(Quantile),] # smallest to largest
+
+  ## ClinVar-quantiled SeqWeaver scores
+  q = sapply(colnames(SW), function(x) {
+    dt = SW[,x,with=FALSE]; setnames(dt, x, 'V')
+    qt = clinvar[, c(x,'Quantile'), with=FALSE]; setnames(qt, x, 'V')
+    qt = qt[dt, on=.(V<V), mult='last'][, Quantile := ifelse(is.na(Quantile), 0, Quantile)]
+    qt[,Quantile]
+  })
+
+  if (!'INCAscore' %in% colnames(variants)) {
+    variants[,'INCAscore'] = 0
+  }
+  variants[, INCAscore := INCAscore + apply(q, 1, max)]
+
+  return(variants)
+}
+
+
+scoreAllelicEffect = function(variants, CellLine1, CellLine2, CellNames, window=15, parallel=FALSE) {
+  variants = as.data.table(variants)
+  enriched = matrix(0, ncol=2, nrow=nrow(variants))
+  ## Cell line 1
+  enriched[varInEnrichedRegion(variants, CellLine1, window, parallel), 1] = 1
+  ## Cell line 2
+  enriched[varInEnrichedRegion(variants, CellLine2, window, parallel), 2] = 1
 
   variants[, 'EnrichedRegion'] = as.character(factor(paste(enriched[,1], enriched[,2], sep=':'),
                                                      levels = c('0:0','0:1','1:0','1:1'),
-                                                     labels = c('Neither',CellLineNames,'Both')))
+                                                     labels = c('Neither',CellNames,'Both')))
+
+  if ('AltCellLine' %in% colnames(variants)) {
+    ALT = variants[,AltCellLine] %in% CellNames
+  } else {
+    ALT = TRUE
+  }
+  if (!'INCAscore' %in% colnames(variants)) {
+    variants[,'INCAscore'] = 0
+  }
+  variants[ALT & EnrichedRegion %in% CellNames, INCAscore := INCAscore + 1]
+
+  return(variants)
+}
+
+
+scoreVarImpactOnGE = function(variants, CellLine1, CellLine2, CellNames, window=15, parallel=FALSE) {
+  variants = as.data.table(variants)
+
+  ## One gene in each row
+  variants[, Gene := gsub('\\s+','',Gene)]
+  variants[Gene=='', 'Gene'] = '-'
+  variants = variants[, .(Gene = unlist(strsplit(Gene, '[^[:alnum:]\\-\\_\\.]', perl=TRUE))),
+                      by = eval(grep('^Gene$', colnames(variants), invert=TRUE, value=TRUE))]
+
+  if (!'list' %in% class(CellLine1)) {
+    CellLine1 = list(CellLine1)
+  }
+  if (!'list' %in% class(CellLine2)) {
+    CellLine1 = list(CellLine2)
+  }
+
+  ## (i) The gene displays significant changes in expression upon knockdown in one of the cell lines
+  de = data.table(mapDEResultToGenes(variants, CellLine1[[1]], CellNames[1]), ## Cell line 1
+                  mapDEResultToGenes(variants, CellLine2[[1]], CellNames[2])) ## Cell line 2
+
+  variants[, 'DESignificant'] =
+    as.character(factor(apply(de[,grep('significant',colnames(de)),with=FALSE], 1, paste, collapse=':'),
+                        levels = c('NA:NA','0:0','0:1','1:0','1:1'),
+                        labels = c(NA,'Neither',CellNames,'Both')))
+
+  if ('AltCellLine' %in% colnames(variants)) {
+    ALT = variants[,AltCellLine] %in% CellNames
+  } else {
+    ALT = TRUE
+  }
+  if (!'INCAscore' %in% colnames(variants)) {
+    variants[,'INCAscore'] = 0
+  }
+  variants[ALT & DESignificant %in% CellNames, INCAscore := INCAscore + 1]
+
+  ## (ii) Whether the cell line with the expression change has a peak covering the location of the variant
+  if (length(CellLine1) > 1 & length(CellLine2) > 1) {
+    enriched = matrix(0, ncol=2, nrow=nrow(variants))
+    ## Cell line 1
+    enriched[varInEnrichedRegion(variants, CellLine1[-1], window, parallel), 1] = 1
+    ## Cell line 2
+    enriched[varInEnrichedRegion(variants, CellLine2[-1], window, parallel), 2] = 1
+
+    variants[, 'DEBinding'] = as.character(factor(paste(enriched[,1], enriched[,2], sep=':'),
+                                                  levels = c('0:0','0:1','1:0','1:1'),
+                                                  labels = c('Neither',CellNames,'Both')))
+
+    variants[ALT & DESignificant %in% CellNames & DESignificant == DEBinding, INCAscore := INCAscore + 1]
+  } else if ('EnrichedRegion' %in% colnames(variants)) {
+    variants[ALT & DESignificant %in% CellNames & DESignificant == EnrichedRegion, INCAscore := INCAscore + 1]
+  } else {
+    message('No `peak` or `counts` files provided for assessing binding at the variant location.
+        Max score for RBP-SNV impact on gene expression is 1.')
+  }
   return(variants)
 }
